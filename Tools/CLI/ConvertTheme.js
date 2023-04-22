df20e905ae2c544ef4880e261f7537abb4e43542
Daomtthuan <dao.mt.thuan@gmail.com>
2023-04-23 01:11:43 +0700

    [Daomtthuan] Improve CLI
    Improve ConvertTheme CLI
    Add pre-build event for VS project

--- Tools/CLI/ConvertTheme.js
+++ Tools/CLI/ConvertTheme.js
@@ -1,19 +1,10 @@
 const Path = require('path');
 const Process = require('child_process');
 
-// Build VS ThemeConverter tool
-
-Process.execSync('dotnet build', {
-  cwd: Path.resolve(__dirname, '../../Tools/ThemeConverter'),
-  stdio: 'inherit',
-});
-
 const darkSchemasDir = Path.resolve(__dirname, '../../Src/VS/CodeTheme/Schemas/Dark');
 const darkPackagesDir = Path.resolve(__dirname, '../../Src/VS/CodeTheme/Packages/Dark');
 
-// Build Theme Schemas to VS Packages
-
-// Dark theme
+// Covert Theme Schemas to VS Packages use ThemeConverter tool
 Process.execSync(`ThemeConverter -i "${darkSchemasDir}" -o "${darkPackagesDir}"`, {
   cwd: Path.resolve(__dirname, '../../Tools/ThemeConverter/ThemeConverter/bin/Debug/net6.0'),
   stdio: 'inherit',
